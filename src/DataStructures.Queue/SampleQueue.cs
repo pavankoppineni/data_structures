ce4c21dd40907a972ddda1d5c9ba03863646d05b
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-04-26 23:37:39 +0530

    Added Queue Data structure
    1. Added queue data structure
    2. Added unit tests for queue data structure

---
+++ src/DataStructures.Queue/SampleQueue.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace DataStructures.Queue
+{
+    public class SampleQueue
+    {
+        private readonly int size;
+        private int front;
+        private int rear;
+        private readonly int[] values;
+
+        public SampleQueue(int size)
+        {
+            this.size = size;
+            front = -1;
+            rear = -1;
+            values = new int[this.size];
+        }
+
+        public int Length
+        {
+            get
+            {
+                if (IsEmpty()) return 0;
+                return rear - front + 1;
+            }
+        }
+
+        private bool IsFull()
+        {
+            if (rear >= size - 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsEmpty()
+        {
+            if(front < 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void EnQueue(int value)
+        {
+            if (IsFull())
+            {
+                throw new OverflowException();
+            }
+            rear++;
+            values[rear] = value;
+            if (front == -1)
+            {
+                front = 0;
+            }
+        }
+
+        public int DeQueue()
+        {
+            if (IsEmpty())
+            {
+                throw new IndexOutOfRangeException();
+            }
+            var value = values[front];
+            front++;
+            if (front >= rear)
+            {
+                front = -1;
+                rear = -1;
+            }
+            return value;
+        }
+    }
+}
---
+++ test/DataStructures.Queue.Tests/SampleQueueTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Queue.Tests
+{
+    [TestClass]
+    public class SampleQueueTests
+    {
+        [TestMethod]
+        public void GivenTwoItems_EnQueue_LengthShouldBeTwo()
+        {
+            //Arrange
+            var size = 5;
+            var sampleQueue = new SampleQueue(size);
+
+            //Act
+            sampleQueue.EnQueue(1);
+            sampleQueue.EnQueue(2);
+
+            //Assert
+            Assert.AreEqual(2, sampleQueue.Length);
+        }
+
+        [TestMethod]
+        public void GivenQueueWithFullCapacity_EnQueue_ThrowsOverflowException()
+        {
+            //Arrange
+            var size = 5;
+            var sampleQueue = new SampleQueue(size);
+            sampleQueue.EnQueue(1);
+            sampleQueue.EnQueue(2);
+            sampleQueue.EnQueue(3);
+            sampleQueue.EnQueue(4);
+            sampleQueue.EnQueue(5);
+
+            //Act
+            Action action = () => sampleQueue.EnQueue(6);
+
+            //Assert
+            Assert.ThrowsException<OverflowException>(action);
+        }
+
+        [TestMethod]
+        public void GivenQueueWithLengthFive_DeQueue_TwoItems_LengthShouldBeThree()
+        {
+            //Arrange
+            var size = 5;
+            var sampleQueue = new SampleQueue(size);
+            sampleQueue.EnQueue(1);
+            sampleQueue.EnQueue(2);
+            sampleQueue.EnQueue(3);
+            sampleQueue.EnQueue(4);
+            sampleQueue.EnQueue(5);
+
+            //Act
+            sampleQueue.DeQueue();
+            sampleQueue.DeQueue();
+
+            //Assert
+            Assert.AreEqual(3, sampleQueue.Length);
+        }
+
+        [TestMethod]
+        public void GivenQueueWithLengthThree_DeQueue_FourItems_ThrowsIndexOutOfRangeException()
+        {
+            //Arrange
+            var size = 3;
+            var sampleQueue = new SampleQueue(size);
+            sampleQueue.EnQueue(1);
+            sampleQueue.EnQueue(2);
+            sampleQueue.EnQueue(3);
+
+            //Act
+            sampleQueue.DeQueue();
+            sampleQueue.DeQueue();
+            sampleQueue.DeQueue();
+            
+            Action action = () => sampleQueue.DeQueue();
+
+            //Assert
+            Assert.ThrowsException<IndexOutOfRangeException>(action);
+        }
+    }
+}
