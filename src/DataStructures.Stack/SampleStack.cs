77dc24ab0911cb46f4f0b588253f71c2f243ce3f
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-04-26 23:10:25 +0530

    Stack data structure
    1. Added implementation of stack data structure
    2. Added unit tests for stack data structure

---
+++ src/DataStructures.Stack/SampleStack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Stack
+{
+    public class SampleStack
+    {
+        private readonly int size;
+        private readonly int[] values;
+        private int currentPointer;
+
+        public SampleStack(int size)
+        {
+            this.size = size;
+            values = new int[size];
+            currentPointer = -1;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return currentPointer + 1;
+            }
+        }
+
+        public void Push(int value)
+        {
+            if (currentPointer + 1 >= size)
+            {
+                throw new StackOverflowException();
+            }
+
+            currentPointer++;
+            values[currentPointer] = value;
+        }
+
+        public int Pop()
+        {
+            if (currentPointer < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            var value = values[currentPointer];
+            currentPointer--;
+            return value;
+        }
+    }
+}
---
+++ test/DataStructures.Stack.Tests/SampleStackTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Stack.Tests
+{
+    [TestClass]
+    public class SampleStackTests
+    {
+        [TestMethod]
+        public void GivenTwoItems_Push_CountShouldBeTwo()
+        {
+            //Arrange
+            var size = 5;
+            var sampleStack = new SampleStack(size);
+
+            //Act
+            sampleStack.Push(1);
+            sampleStack.Push(2);
+
+            //Assert
+            Assert.AreEqual(2, sampleStack.Count);
+        }
+
+        [TestMethod]
+        public void GivenStackWithFullCapacity_Push_ShouldThrowStackOverflowException()
+        {
+            //Arrange
+            var size = 5;
+            var sampleStack = new SampleStack(size);
+            sampleStack.Push(1);
+            sampleStack.Push(2);
+            sampleStack.Push(3);
+            sampleStack.Push(4);
+            sampleStack.Push(5);
+
+            //Act
+            Action action = () => sampleStack.Push(6);
+
+            //Assert
+            Assert.ThrowsException<StackOverflowException>(action);
+        }
+
+        [TestMethod]
+        public void GivenStackWithLengthFive_Pop_TwoItems_LengthOfStackShouldBeThree()
+        {
+            //Arrange
+            var size = 5;
+            var sampleStack = new SampleStack(size);
+            sampleStack.Push(1);
+            sampleStack.Push(2);
+            sampleStack.Push(3);
+            sampleStack.Push(4);
+            sampleStack.Push(5);
+
+            //Act
+            sampleStack.Pop();
+            sampleStack.Pop();
+
+            //Assert
+            Assert.AreEqual(3, sampleStack.Count);
+        }
+
+        [TestMethod]
+        public void GivenStackWithLengthZero_Pop_ShouldThrowIndexOutOfRangeException()
+        {
+            //Arrange
+            var size = 0;
+            var sampleStack = new SampleStack(size);
+
+            //Act
+            Action action = () => sampleStack.Pop();
+
+            //Assert
+            Assert.ThrowsException<IndexOutOfRangeException>(action);
+        }
+    }
+}
