90c85d6cb1d59c66633f8eaded696e1d895702f6
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-05-10 00:11:26 +0530

    Added Segment Tree Data Structure

---
+++ test/DataStructures.Tests/Tree/SegmentTree/MinimumValueSegmentTreeTests.cs
@@ -0,0 +1,26 @@
+﻿using DataStructures.Tree.SegmentTree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Tests.Tree.SegmentTree
+{
+    [TestClass]
+    public class MinimumValueSegmentTreeTests
+    {
+        [TestMethod]
+        public void ArrayWithSizeFour_ConstructTree_ReturnsSegmentTreeArray()
+        {
+            //Arrange
+            var values = new int[] { 1, 2, 3, 4 };
+            var segmentTree = new MinimumValueSegmentTree();
+
+            //Act
+            var segmentTreeArray = segmentTree.ConstructTree(values);
+
+            //Assert
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
