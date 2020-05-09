9c180b617b62bfcbe2787da2673c244206892372
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-05-10 00:14:43 +0530

    Added segment tree data structure (#8)
    * Stack data structure
    
    1. Added implementation of stack data structure
    2. Added unit tests for stack data structure
    
    * Added Queue Data structure
    
    1. Added queue data structure
    2. Added unit tests for queue data structure
    
    * Fixed unit testing issue
    
    * Added code to test GitHub build actions
    
    * Fixed code
    
    * Added new project
    
    * Added Segment Tree Data Structure
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ src/DataStructures/Tree/SegmentTree/MinimumValueSegmentTree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Tree.SegmentTree
+{
+    /// <summary>
+    /// Segment Tree Data Structure
+    /// </summary>
+    public class MinimumValueSegmentTree
+    {
+        public int[] ConstructTree(int[] values)
+        {
+            var segmentArray = new int[7];
+            ConstructTree(values, segmentArray, 0, values.Length - 1, 0);
+            return segmentArray;
+        }
+
+        private void ConstructTree(int[] values, int[] segmentArray, int low, int high, int position)
+        {
+            if (low == high)
+            {
+                segmentArray[position] = values[low];
+                return;
+            }
+            var mid = (low + high) / 2;
+            var left = (2 * position) + 1;
+            var right = (2 * position) + 2;
+            ConstructTree(values, segmentArray, low, mid, left);
+            ConstructTree(values, segmentArray, mid + 1, high, right);
+            segmentArray[position] = Math.Min(segmentArray[left], segmentArray[right]);
+        }
+    }
+}
