44a5e9e24577ab211f74ee0732e3915ca870201c
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-06-07 17:49:24 +0530

    Added max heap and min heap

---
+++ src/DataStructures/Heap/MaxHeapV1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Heap
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/heap-data-structure/
+    /// </summary>
+    public class MaxHeapV1
+    {
+        private int[] values;
+        private int length;
+
+        public MaxHeapV1(int[] values)
+        {
+            this.values = values;
+            this.length = this.values.Length;
+        }
+
+        public void Heapify()
+        {
+            for (var i = length / 2; i >= 0; i--)
+            {
+                Heapify(i);
+            }
+        }
+
+        public int Peek()
+        {
+            return values[0];
+        }
+
+        private void Heapify(int index)
+        {
+            var left = 2 * index + 1;
+            var right = 2 * index + 2;
+            var maxIndex = index;
+            var maxValue = values[index];
+            if (left < length)
+            {
+                if (maxValue < values[left])
+                {
+                    maxValue = values[left];
+                    maxIndex = left;
+                }
+            }
+
+            if (right < length)
+            {
+                if (maxValue < values[right])
+                {
+                    maxValue = values[right];
+                    maxIndex = right;
+                }
+            }
+
+            if (index != maxIndex)
+            {
+                var temp = values[index];
+                values[index] = values[maxIndex];
+                values[maxIndex] = temp;
+                Heapify(maxIndex);
+            }
+        }
+    }
+}
---
+++ src/DataStructures/Heap/MinHeapV1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Heap
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MinHeapV1
+    {
+        private int[] values;
+        private int length;
+
+        public MinHeapV1(int[] values)
+        {
+            this.values = values;
+            this.length = this.values.Length;
+        }
+
+        public void Heapify()
+        {
+            for (var i = length / 2; i >= 0; i--)
+            {
+                Heapify(i);
+            }
+        }
+
+        public int Peek()
+        {
+            return values[0];
+        }
+
+        private void Heapify(int index)
+        {
+            var left = 2 * index + 1;
+            var right = 2 * index + 2;
+            var minIndex = index;
+            var minValue = values[index];
+            if (left < length)
+            {
+                if (minValue > values[left])
+                {
+                    minValue = values[left];
+                    minIndex = left;
+                }
+            }
+
+            if (right < length)
+            {
+                if (minValue > values[right])
+                {
+                    minValue = values[right];
+                    minIndex = right;
+                }
+            }
+
+            if (index != minIndex)
+            {
+                var temp = values[index];
+                values[index] = values[minIndex];
+                values[minIndex] = temp;
+                Heapify(minIndex);
+            }
+        }
+    }
+}
