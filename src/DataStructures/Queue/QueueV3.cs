37b752a401540220854780b7a8fd110bd71d17b3
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-06-11 16:41:45 +0530

    Added queue data structure (#14)
    * Stack data structure
    
    1. Added implementation of stack data structure
    2. Added unit tests for stack data structure
    
    * Added Queue Data structure
    
    1. Added queue data structure
    2. Added unit tests for queue data structure
    
    * Fixed unit testing issue
    
    * Added code to test GitHub build actions
    
    * Fixed code
    
    * Added new project
    
    * Added Segment Tree Data Structure
    
    * Added circular queue data structure
    
    * Added circular queue
    
    * Fixed unit test for double ended queue
    
    * Added max heap and min heap
    
    * Changed README
    
    * Added BinarySearchTree datastructure
    
    * Added unit tests
    
    * Added queue data structure
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ src/DataStructures/Queue/QueueV3.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Queue
+{
+    public class QueueV3
+    {
+        private readonly int length;
+        private int[] values;
+        private int front;
+        private int end;
+
+        public QueueV3(int length)
+        {
+            this.length = length;
+            this.values = new int[length];
+            this.front = -1;
+            this.end = -1;
+        }
+
+        public void EnQueue(int value)
+        {
+            if (front == 0 && end == length - 1)
+            {
+                throw new OverflowException();
+            }
+
+            end = end + 1;
+            values[end] = value;
+            if (front < 0)
+            {
+                front = 0;
+            }
+        }
+
+        public int DeQueue()
+        {
+            if (front < 0)
+            {
+                throw new Exception("Queue is empty");
+            }
+
+            var value = values[front];
+            if (front == end)
+            {
+                front = -1;
+                end = -1;
+            }
+            else
+            {
+                front = front + 1;
+            }
+            return value;
+        }
+    }
+}
---
+++ test/DataStructures.Tests/Queue/QueueV3Tests.cs
@@ -0,0 +1,41 @@
+﻿using DataStructures.Queue;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Tests.Queue
+{
+    [TestClass]
+    public class QueueV3Tests
+    {
+        [TestMethod]
+        public void Given_Queue_When_AddElementsAndDeleteElements_Then_ElementsShouldBeRemovedInOrderTheyAreAdded()
+        {
+            //Given
+            var length = 5;
+            var queue = new QueueV3(length);
+            var expectedValues = new int[] { 10, 19, 8, 9, 1 };
+            var actualValues = new List<int>();
+
+            //When
+            foreach (var value in expectedValues)
+            {
+                queue.EnQueue(value);
+            }
+
+            foreach (var value in expectedValues)
+            {
+                actualValues.Add(queue.DeQueue());
+            }
+
+            //Then
+            for (var i = 0; i < expectedValues.Length; i++)
+            {
+                var expectedValue = expectedValues[i];
+                var actualValue = actualValues[i];
+                Assert.AreEqual(expectedValue, actualValue);
+            }
+        }
+    }
+}
