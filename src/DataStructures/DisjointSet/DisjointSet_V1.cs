ea90fa7d550cc1e78977f99997129954af73c24a
pkoppineni <pkoppineni@csod.com>
2021-09-11 18:26:42 +0530

    Added disjoint-set data structure

---
+++ src/DataStructures/DisjointSet/DisjointSet_V1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.DisjointSet
+{
+    /// <summary>
+    /// https://www.hackerearth.com/practice/notes/disjoint-set-union-union-find/
+    /// </summary>
+    public class DisjointSet_V1
+    {
+        private readonly int[] _values;
+        public DisjointSet_V1(int n)
+        {
+            _values = new int[n];
+            for (var index = 0; index < n; index++)
+            {
+                _values[index] = index;
+            }
+        }
+
+        public void Union(int a, int b)
+        {
+            var rootA = _values[a];
+            for (var i = 0; i < _values.Length; i++)
+            {
+                if (_values[i] == rootA)
+                {
+                    _values[i] = _values[b];
+                }
+            }
+        }
+
+        public bool Find(int a, int b)
+        {
+            var rootA = _values[a];
+            var rootB = _values[b];
+            return rootA == rootB;
+        }
+    }
+}
---
+++ test/DataStructures.Tests/DisjointSet/DisjointSet_V1Tests.cs
@@ -0,0 +1,31 @@
+﻿using DataStructures.DisjointSet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Tests.DisjointSet
+{
+    [TestClass]
+    public class DisjointSet_V1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_ConstructDisjointSet_Then_ShouldGenerateDisjointSet()
+        {
+            //Given
+            var disjointSet = new DisjointSet_V1(4);
+            var expectedResult_13 = true;
+            var expectedResult_02 = false;
+
+            //When
+            disjointSet.Union(0, 3);
+            disjointSet.Union(1, 3);
+            var actualResult_02 = disjointSet.Find(0, 2);
+            var actualResult_13 = disjointSet.Find(1, 3);
+
+            //Then
+            Assert.AreEqual(expectedResult_02, actualResult_02);
+            Assert.AreEqual(expectedResult_13, actualResult_13);
+        }
+    }
+}
