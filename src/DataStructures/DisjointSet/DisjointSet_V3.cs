4b903f21d812e61fdf92b425c2f29c7ea2e6fd9d
pkoppineni <pkoppineni@csod.com>
2021-11-08 21:43:48 +0530

    Added disjoint set

---
+++ src/DataStructures/DisjointSet/DisjointSet_V3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.DisjointSet
+{
+    public class DisjointSet_V3
+    {
+        private int[] _values;
+        public DisjointSet_V3(int[] values)
+        {
+            _values = values;
+        }
+
+        private int GetParent(int a)
+        {
+            while (_values[a] != a)
+            {
+                a = _values[a];
+            }
+            return a;
+        }
+
+        public void Union(int a, int b)
+        {
+            var parentA = GetParent(a);
+            var parentB = GetParent(b);
+            _values[parentA] = _values[parentB];
+        }
+
+        public bool Find(int a, int b)
+        {
+            var parentA = GetParent(a);
+            var parentB = GetParent(b);
+            return parentA == parentB;
+        }
+    }
+}
---
+++ test/DataStructures.Tests/DisjointSet/DisjointSet_V3Tests.cs
@@ -0,0 +1,29 @@
+﻿using DataStructures.DisjointSet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Tests.DisjointSet
+{
+    [TestClass]
+    public class DisjointSet_V3Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_ConstructDisjointSet_Then_ShouldReturnDisjointSet()
+        {
+            //Given
+            var values = new int[] { 0, 1, 2, 3, 4 };
+            var disjointSet = new DisjointSet_V3(values);
+
+            //When
+            disjointSet.Union(0, 4);
+            disjointSet.Union(1, 4);
+            disjointSet.Union(1, 2);
+
+            //Then
+            Assert.AreEqual(true, disjointSet.Find(0, 2));
+            Assert.AreEqual(false, disjointSet.Find(1, 3));
+        }
+    }
+}
